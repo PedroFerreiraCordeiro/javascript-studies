50f4e43baac65acd8b27baabec474629b081a486
Sistemas Tecnocomp <sistemas.tecnocomp@tecnocomp.com.br>
2023-01-16 22:29:21 -0300

    Code Ready / finally course

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 
 function tocaSom(idElementoAudio) {
-    document.querySelector(idElementoAudio).play();
+   const element =  document.querySelector(idElementoAudio);
+   if(element === null){
+    alert('elemento não encontrado')
+   }if(element != null && element.localName === 'audio'){
+
+   element.play();
+   }else{
+    console.log('elemento não encontrado');
+   }
+ 
 }
-const listaTeclas = document.querySelectorAll('.tecla');
+const listaTeclas = document.querySelectorAll('.tecla'); 
 
-let contador = 0; 
-
-while (contador < listaTeclas.length) {
+for (let contador = 0; contador < listaTeclas.length; contador ++) {
     const tecla = listaTeclas[contador];
     const instrumento = tecla.classList[1];
-
-    console.log(instrumento);
     const idAudio =  `#som_${instrumento}`
+
     tecla.onclick = function () {
         tocaSom(idAudio);
     }
-    contador = contador + 1;
-    console.log(contador); 
+
+    tecla.onkeydown = function (event, ){
+        console.timeLog(event.code)
+        if(event.code == 'backspace' || event.code == 'enter'){
+        tecla.classList.add('ativa');
+        }
+        
+    }
+
+    tecla.onkeyup = function() {
+        if(tecla.onkeydown == 'backspace' || tecla.onkeydown == 'enter'){
+        tecla.classList.remove('ativa');
+    
+         }
+    }
+
+    
 }
